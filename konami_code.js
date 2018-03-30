8bc29465d9249449d73fbf23a3ebf0f32bb65941
Monica Seaman <monica.l.seaman@gmail.com>
2018-03-30 19:08:56 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,12 +1,20 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
+let index = 0;
+
 function init() {
   const body = document.body;
   body.addEventListener('keydown', function(e) {
-    var keysEntered = [];
-    keysEntered = keysEntered + e.which;
-  });
-  if(keysEntered === code) {
-    alert("You know the super-secret password!!")
+    var keys = parseInt(e.detail || e.which);
+    if (keys === code[index]) {
+      index++;
+      if (index === code.length) {
+        alert("You know the super-secret password!!");
+        index = 0;
+      }
+    } else {
+      index = 0;
+    }
+   
   }
-}
+})
