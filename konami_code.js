2c0b76a7b8e97751b09022a1187a4663ea3adee3
Monica Seaman <monica.l.seaman@gmail.com>
2018-03-30 19:01:58 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,10 +2,11 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   const body = document.body;
-  body.addEventListener('keydown', function (e) {
+  body.addEventListener('keydown', function(e) {
     var keysEntered = [];
-    keysEntered = keysEntered + e.which
-    if(e.which ===
-  })
-
+    keysEntered = keysEntered + e.which;
+  });
+  if(keysEntered === code) {
+    alert("You know the super-secret password!!")
+  }
 }
