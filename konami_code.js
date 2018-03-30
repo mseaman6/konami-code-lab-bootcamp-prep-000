af3a9be9ec1d7594252fb3ae4e8dfb1aff7b1f02
Monica Seaman <monica.l.seaman@gmail.com>
2018-03-30 19:47:01 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -10,7 +10,7 @@ function init() {
 function codeListener(e) {
   console.log(e.which);
   debugger;
-//  var key = parseInt(e.which);
+    var key = parseInt(e.which);
 //  if (key === code[index]) {
 //    index++;
 //    if (index === code.length) {
