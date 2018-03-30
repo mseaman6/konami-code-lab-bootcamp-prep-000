82fe7968bfc0619dac8c8e831f288688cb0af496
Monica Seaman <monica.l.seaman@gmail.com>
2018-03-30 19:29:55 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -3,7 +3,7 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 let index = 0;
 
 function init() {
-  const body = document.body;
+  const body = document.querySelector('body');
   body.addEventListener('keydown', codeListener());
 }
 
